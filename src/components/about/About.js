07eb6a069b095fb9488620b25b532646a21a3262
mauricioc12 <mauriciocontrerasr1202@gmail.com>
2022-10-19 23:11:40 -0500

    About changed

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,6 +8,8 @@ const About = () => {
       <div className='about-desc'>
         <h3>Let me tell you something about me:</h3>
         <p>Innovative and proactive Web Development student with a strong interest in Frontend and Backend. I have great ability to develop elegant web pages and application designs for demanding clients, to adapt to all kinds of environments and always bring the best of me. my best. I am characterized by my facility for teamwork, my enthusiasm for learning and developing my skills. I am looking for a job opportunity in which to gain more experience.</p>
+        <h4>Skills:</h4>
+        <p className='about-desc--p'>HTML(Pug), CSS(Sass, Stylus, Less), JavaScript, ReactJS, API Rest, Tailwind 2 y 3.</p>
       </div>
       <div className='about-img'>
         <img src={fotoMe} alt='About'/>
